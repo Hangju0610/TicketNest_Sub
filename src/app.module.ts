545394f8397924523eb7130da5493dbbdc9a53b2
hangju0610 <phj950610@naver.com>
2023-08-25 10:18:17 +0900

    Feat : Add booking module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { BookingModule } from './booking/booking.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    BookingModule,
   ],
   controllers: [AppController],
   providers: [AppService],
